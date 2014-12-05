cbca0af9373df16adbd1d208760e9a233a2e7400
micko189 <ajdinovic@gmail.com>
2014-12-05 16:42:00 +0100

    Added font generation

---
+++ FontsBDFgen/Font_gen.bat
@@ -0,0 +1,6 @@
+bdf2u8g_101.exe -b 47 -e 122 helvB10.bdf helvB10r helvB10r.c
+bdf2u8g_101.exe -b 48 -e 176 helvB12.bdf helvB12r helvB12r.c
+bdf2u8g_101.exe -b 48 -e 122 helvB14.bdf helvB14r helvB14r.c
+bdf2u8g_101.exe -b 48 -e 58 helvB18.bdf helvB18r helvB18r.c
+bdf2u8g_101.exe -b 48 -e 58 helvB24.bdf helvB24r helvB24r.c
+COPY helvB10r.c+helvB12r.c+helvB14r.c+helvB18r.c+helvB24r.c helvBr.c
